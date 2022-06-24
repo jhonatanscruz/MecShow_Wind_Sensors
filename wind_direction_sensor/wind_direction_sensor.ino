ea23e42ca16759d861afe79be4ad7d7634da84e9
Jhonatan da Silva Cruz <jhonatan.s.batista@gmail.com>
2022-06-24 17:27:19 -0300

    Code for BME680 Sensor provided

--- wind_direction_sensor/wind_direction_sensor.ino
+++ wind_direction_sensor/wind_direction_sensor.ino
@@ -1,12 +1,25 @@
+/*
+ * Code to read Mikroe Environment Click(BME680 Sensor)
+ * and Campbell 034B Wind Set
+ * All data is sent to Gateway by Xbee
+ * 
+ * Autor: Jhonatan da Silva Cruz
+ * From FTTECH Software Team
+ * 
+ * Last Update: 2022-06-24
+ */
 #include "FTTech_SAMD51Clicks.h"
-// XBEE 2
 #include "FTTech_Xbee.h"
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
 
 #define PIN_CH1_4a20 A1 // Wind direction
 #define PIN_CH2_4a20 A0 // Wind Speed
 #define DEBUG false
 
 int BAUDRATE = 115200;
+Adafruit_BME680 bme; // I2C
 
 /* ***************************************************************************
  *            INTERVALO
@@ -22,13 +35,24 @@ int BAUDRATE = 115200;
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 void setup() {
-  digitalWrite(52, HIGH); //Put Click 4 ON
   //4-20 Click uses external feeding, so
   //you doesn't need to power it with the click output.
+  digitalWrite(52, HIGH); //Put Click 4 ON
+  FTClicks.turnON_5V(); // Enable 5V output
+  FTClicks.turnON(2);   // Enable the 3.3V output for Click 2
   Serial.begin(9600);
   Serial4.begin(BAUDRATE);
   digitalWrite(A9, HIGH); //Xbee Reset
-  xbee.begin(BAUDRATE);
+  delay(100);
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    while (1);
+  }
+  // Set up oversampling and filter initialization for BME680
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+
   Serial.println(F("*****************INICIOU*****************"));
   delay(100);
 }
@@ -47,7 +71,7 @@ void loop() {
     uint8_t aux = 0;
     float CH1_4a20; // Wind direction
     float CH2_4a20; // Wind speed
-    
+
     while(aux < 3){
       dir_temp[aux] = analogRead(PIN_CH1_4a20) * (3.3/1023);
       spd_temp[aux] = analogRead(PIN_CH2_4a20) * (3.3/1023);
@@ -64,9 +88,12 @@ void loop() {
 
   // ==========================================================================================
 
-
     // save the last time reading
     previousMillis = currentMillis;
+
+    if (!bme.performReading()) {
+      Serial.println("Failed to perform reading :(");
+    }
 
     if(DEBUG){
       Serial.println("Canal 1: ");
@@ -75,6 +102,18 @@ void loop() {
       Serial.println("Canal 2 : ");
       Serial.print("\tTensão: "); Serial.println(CH2_4a20);
       Serial.println("SPEED: " + String(getWindSpeed(CH2_4a20),3));
+      
+      Serial.print("Temperature = ");
+      Serial.print(bme.temperature);
+      Serial.println(" *C");
+
+      Serial.print("Pressure = ");
+      Serial.print(bme.pressure / 100.0);
+      Serial.println(" hPa");
+
+      Serial.print("Humidity = ");
+      Serial.print(bme.humidity);
+      Serial.println(" %");
     }
 
     else{
@@ -84,14 +123,9 @@ void loop() {
         Serial4.write(sensor_dir[i]);
       }
       Serial4.flush();
-
       Serial.println("Enviado DIR");
-
       delay(100);
 
-      // save the last time reading
-      previousMillis = currentMillis;
-      
       // ===== Send Wind Speed to Xbee =====
       String sensor_spd = "FTMS,SPD," + String(getWindSpeed(CH2_4a20),2);
       for(int i = 0; i < sensor_spd.length(); i++){
@@ -99,6 +133,33 @@ void loop() {
       }
       Serial4.flush();
       Serial.println("Enviado SPD");
+      delay(100);
+
+      // ===== Send BME680 TEMPERATURE to Xbee =====
+      String BME_TEMP = "FTMS,TEM," + String(bme.temperature,2);
+      for(int i = 0; i < sensor_dir.length(); i++){
+        Serial4.write(BME_TEMP[i]);
+      }
+      Serial4.flush();
+      Serial.println("Enviado BME_TEMP");
+      delay(100);
+
+      // ===== Send BME680 PRESSURE to Xbee =====
+      String BME_PRESS = "FTMS,PRE," + String(bme.pressure / 100.0,2);
+      for(int i = 0; i < sensor_dir.length(); i++){
+        Serial4.write(BME_PRESS[i]);
+      }
+      Serial4.flush();
+      Serial.println("Enviado BME_PRESS");
+      delay(100);
+
+      // ===== Send BME680 HUMIDITY to Xbee =====
+      String BME_HUM = "FTMS,HUM," + String(bme.humidity,2);
+      for(int i = 0; i < sensor_dir.length(); i++){
+        Serial4.write(BME_HUM[i]);
+      }
+      Serial4.flush();
+      Serial.println("Enviado BME_HUM");
     }
   }
 }
